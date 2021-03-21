7df36254f80a104751410ce8ae43e8f2e37ff7d4
tschumann <schumann.tom@gmail.com>
2021-03-21 22:03:25 +1000

    Started adding unit tests.

---
+++ config-test.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace configtest
+{
+	TEST_CLASS(configtest)
+	{
+	public:
+		
+		TEST_METHOD(TestTrace)
+		{
+		}
+	};
+}
