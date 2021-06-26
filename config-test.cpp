8e3816cafb9b2da3b414a77b724934c03467c207
tschumann <schumann.tom@gmail.com>
2021-06-26 18:26:08 +1000

    Fixed binary not working due to dependency on Microsoft.VisualStudio.TestTools.CppUnitTestFramework.x64.dll

--- config-test.cpp
+++ config-test.cpp
@@ -3,12 +3,15 @@
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
+#undef TEST_METHOD
+#define TEST_METHOD(methodName) static const void* test##methodName() { return nullptr; } void methodName()
+
 namespace configtest
 {
 	TEST_CLASS(configtest)
 	{
 	public:
-		
+
 		TEST_METHOD(TestTrace)
 		{
 			Config::Trace( "blah" );
