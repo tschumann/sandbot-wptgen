46fcc4000a1fe49f5de50f1c48ec1e3f8ea43364
tschumann <schumann.tom@gmail.com>
2022-01-04 09:51:42 +1000

    Made test.sh run on Windows.

--- test.sh
+++ test.sh
@@ -4,5 +4,41 @@ set -eu
 
 cd $(dirname "${BASH_SOURCE[0]}")
 
-./sandbot-wptgen bsp/bounce.bsp
-bounce_checksum=$(md5sum bounce.wpt)
+bin_path="./sandbot-wptgen"
+failed=0
+
+# TODO: is this right? might be better to check for Linux as Git Bash on Windows gives MINGW64_NT-10.0-19043
+if [[ $(uname) == *"NT"* ]]; then
+	echo "Running on Windows"
+	bin_path="./Debug/sandbot-wptgen"
+fi
+
+$bin_path -w128 bsp/bounce.bsp > /dev/null
+checksum=$(md5sum bounce.wpt | awk '{ print $1 }')
+
+if [ $checksum != "925ab0267d50dbc2aa7e04c2819999f6" ]; then
+	echo "Got checksum $checksum for bounce.wpt"
+	failed=1
+fi
+
+$bin_path -w128 bsp/frenzy.bsp > /dev/null
+checksum=$(md5sum frenzy.wpt | awk '{ print $1 }')
+
+if [ $checksum != "624ef56371c50b5740e567882d865c37" ]; then
+	echo "Got checksum $checksum for frenzy.wpt"
+	failed=1
+fi
+
+$bin_path -w128 bsp/op4_bootcamp.bsp > /dev/null
+checksum=$(md5sum op4_bootcamp.wpt | awk '{ print $1 }')
+
+if [ $checksum != "856e39c4819fc52db43feb8ee8f435ae" ]; then
+	echo "Got checksum $checksum for op4_bootcamp.wpt"
+	failed=1
+fi
+
+echo Removing .wpt files
+rm *.wpt
+
+echo "Exit code $failed"
+exit $failed
