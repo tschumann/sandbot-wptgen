ced1834c8035c7302bb0acf4e35352bf3ed592be
tschumann <schumann.tom@gmail.com>
2022-04-23 17:43:06 +1000

    Updated test script for Windows.

--- tests/test.sh
+++ tests/test.sh
@@ -8,11 +8,12 @@ bin_path="./sandbot-wptgen"
 failed=0
 
 # TODO: is this right? might be better to check for Linux as Git Bash on Windows gives MINGW64_NT-10.0-19043
-if [[ $(uname) == *"NT"* ]]; then
+if [[ $(uname) == *"NT-"* ]]; then
 	echo "Running on Windows"
-	bin_path="./Debug/sandbot-wptgen"
+	bin_path="../Debug/sandbot-wptgen"
 fi
 
+echo "Generating waypoint for bsp/bounce.bsp"
 $bin_path -w128 bsp/bounce.bsp > /dev/null
 checksum=$(md5sum bounce.wpt | awk '{ print $1 }')
 
@@ -21,6 +22,7 @@ if [ $checksum != "925ab0267d50dbc2aa7e04c2819999f6" ]; then
 	failed=1
 fi
 
+echo "Generating waypoint for bsp/frenzy.bsp"
 $bin_path -w128 bsp/frenzy.bsp > /dev/null
 checksum=$(md5sum frenzy.wpt | awk '{ print $1 }')
 
@@ -29,6 +31,7 @@ if [ $checksum != "624ef56371c50b5740e567882d865c37" ]; then
 	failed=1
 fi
 
+echo "Generating waypoint for bsp/op4_bootcamp.bsp"
 $bin_path -w128 bsp/op4_bootcamp.bsp > /dev/null
 checksum=$(md5sum op4_bootcamp.wpt | awk '{ print $1 }')
 
