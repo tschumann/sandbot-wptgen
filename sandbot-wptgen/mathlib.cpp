0a815e6342d9f87a7440310dc3b57fa942fab1b6
tschumann <schumann.tom@gmail.com>
2025-04-12 13:32:20 +1000

    Remove further unused functions from mathlib.

--- sandbot-wptgen/mathlib.cpp
+++ sandbot-wptgen/mathlib.cpp
@@ -43,70 +43,6 @@ double VectorLength(vec3_t v)
 	return length;
 }
 
-
-int VectorCompare (vec3_t v1, vec3_t v2)
-{
-	int		i;
-	
-	for (i=0 ; i<3 ; i++)
-		if (fabs(v1[i]-v2[i]) > EQUAL_EPSILON)
-			return false;
-			
-	return true;
-}
-
-vec_t Q_rint (vec_t in)
-{
-	return floor (in + 0.5);
-}
-
-void VectorMA (vec3_t va, double scale, vec3_t vb, vec3_t vc)
-{
-	vc[0] = va[0] + scale*vb[0];
-	vc[1] = va[1] + scale*vb[1];
-	vc[2] = va[2] + scale*vb[2];
-}
-
-void CrossProduct (vec3_t v1, vec3_t v2, vec3_t cross)
-{
-	cross[0] = v1[1]*v2[2] - v1[2]*v2[1];
-	cross[1] = v1[2]*v2[0] - v1[0]*v2[2];
-	cross[2] = v1[0]*v2[1] - v1[1]*v2[0];
-}
-
-vec_t _DotProduct (vec3_t v1, vec3_t v2)
-{
-	return v1[0]*v2[0] + v1[1]*v2[1] + v1[2]*v2[2];
-}
-
-void _VectorSubtract (vec3_t va, vec3_t vb, vec3_t out)
-{
-	out[0] = va[0]-vb[0];
-	out[1] = va[1]-vb[1];
-	out[2] = va[2]-vb[2];
-}
-
-void _VectorAdd (vec3_t va, vec3_t vb, vec3_t out)
-{
-	out[0] = va[0]+vb[0];
-	out[1] = va[1]+vb[1];
-	out[2] = va[2]+vb[2];
-}
-
-void _VectorCopy (vec3_t in, vec3_t out)
-{
-	out[0] = in[0];
-	out[1] = in[1];
-	out[2] = in[2];
-}
-
-void _VectorScale (vec3_t v, vec_t scale, vec3_t out)
-{
-	out[0] = v[0] * scale;
-	out[1] = v[1] * scale;
-	out[2] = v[2] * scale;
-}
-
 vec_t VectorNormalize (vec3_t v)
 {
 	int		i;
@@ -127,61 +63,6 @@ i=1;
 
 	return length;
 }
-
-void VectorInverse (vec3_t v)
-{
-	v[0] = -v[0];
-	v[1] = -v[1];
-	v[2] = -v[2];
-}
-
-
-void AngleMatrix (const vec3_t angles, float (*matrix)[4] )
-{
-	float		angle;
-	float		sr, sp, sy, cr, cp, cy;
-	
-	angle = angles[2] * (Q_PI*2 / 360);
-	sy = sin(angle);
-	cy = cos(angle);
-	angle = angles[1] * (Q_PI*2 / 360);
-	sp = sin(angle);
-	cp = cos(angle);
-	angle = angles[0] * (Q_PI*2 / 360);
-	sr = sin(angle);
-	cr = cos(angle);
-
-	// matrix = (Z * Y) * X
-	matrix[0][0] = cp*cy;
-	matrix[1][0] = cp*sy;
-	matrix[2][0] = -sp;
-	matrix[0][1] = sr*sp*cy+cr*-sy;
-	matrix[1][1] = sr*sp*sy+cr*cy;
-	matrix[2][1] = sr*cp;
-	matrix[0][2] = (cr*sp*cy+-sr*-sy);
-	matrix[1][2] = (cr*sp*sy+-sr*cy);
-	matrix[2][2] = cr*cp;
-	matrix[0][3] = 0.0;
-	matrix[1][3] = 0.0;
-	matrix[2][3] = 0.0;
-}
-
-
-void VectorRotate (const vec3_t in1, const float in2[3][4], vec3_t out)
-{
-	out[0] = DotProduct(in1, in2[0]);
-	out[1] = DotProduct(in1, in2[1]);
-	out[2] = DotProduct(in1, in2[2]);
-}
-
-
-void VectorTransform (const vec3_t in1, const float in2[3][4], vec3_t out)
-{
-	out[0] = DotProduct(in1, in2[0]) + in2[0][3];
-	out[1] = DotProduct(in1, in2[1]) +	in2[1][3];
-	out[2] = DotProduct(in1, in2[2]) +	in2[2][3];
-}
-
 
 // WARNING!!! the "vector" parameter should be NORMALIZED!!!
 float DistanceToIntersection(const vec3_t origin, const vec3_t vector,
